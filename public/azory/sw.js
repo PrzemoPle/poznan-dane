c47e17e0fc7d6404e47c9d50e07e444640d3291a
Przemo Plewiński <149937384+PrzemoPle@users.noreply.github.com>
2026-08-20 23:51:26 +0200

    Service worker v3 (odswiezenie cache po aktualizacji)

--- public/azory/sw.js
+++ public/azory/sw.js
@@ -1,4 +1,4 @@
-var C='sao-miguel-v2';
+var C='sao-miguel-v3';
 self.addEventListener('install',function(e){self.skipWaiting()});
 self.addEventListener('activate',function(e){
   e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}));
